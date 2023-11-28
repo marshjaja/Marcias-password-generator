bd8e4e06f77134a12740b35bef6ee42eca313a49
marshjaja <114920895+marshjaja@users.noreply.github.com>
2023-11-28 18:57:03 +0000

    Add code for second function for getting a random element from an array

--- script.js
+++ script.js
@@ -118,7 +118,10 @@ function getPasswordOptions() {
 }
 
 // Function for getting a random element from an array
-function getRandom(arr) {}
+function getRandom(arr) {
+	var randomIndex = Math.floor(Math.random() * arr.length);
+	return arr[randomIndex];
+}
 
 // Function to generate password with user input
 function generatePassword() {}
